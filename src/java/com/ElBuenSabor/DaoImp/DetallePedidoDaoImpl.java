59799e84329a27ae5fee3199a2c8e44055bb9741
paul351 <38229848+paul351@users.noreply.github.com>
2018-06-28 03:44:41 -0500

    cocinero uwu

--- src/java/com/ElBuenSabor/DaoImp/DetallePedidoDaoImpl.java
+++ src/java/com/ElBuenSabor/DaoImp/DetallePedidoDaoImpl.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ElBuenSabor.DaoImp;
 
-/**
- *
- * @author Diego
- */
-public class DetallePedidoDaoImpl {
+import com.ElBuenSabor.Dao.Detalle_PedidoDao;
+import com.ElBuenSabor.Entity.Detalle_Pedido;
+import com.ElBuenSabor.Util.Conexion;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DetallePedidoDaoImpl implements Detalle_PedidoDao{
+
     
+    private CallableStatement cst;
+    private ResultSet rs;
+    private Connection cx;
+    
+    @Override
+    public List<Detalle_Pedido> readAll() {
+        
+    List<Detalle_Pedido> datos = new ArrayList<>();
+        try {
+            cx = Conexion.getConexion();
+            cst = cx.prepareCall("{call listarRol()}");
+            rs = cst.executeQuery();
+            while(rs.next()){
+                Detalle_Pedido dp = new Detalle_Pedido();
+                /*dp.setIdr(rs.getInt("idrol"));
+                dp.setNomrol(rs.getString("nom_rol"));*/
+                datos.add(dp);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: "+e);
+        }
+        return datos;
+    
+    }
+    
+  
 }
--- src/java/com/ElBuenSabor/Entity/Detalle_Pedido.java
+++ src/java/com/ElBuenSabor/Entity/Detalle_Pedido.java
@@ -1,14 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ElBuenSabor.Entity;
 
-/**
- *
- * @author Diego
- */
 public class Detalle_Pedido {
+    private int id_det_pedido;
+    private int pedido_id_pedido;
+    private int producto_id_producto;
+    private int promociones_id_promociones;
+    private int cantidad;
+
+    public Detalle_Pedido(int pedido_id_pedido, int producto_id_producto, int promociones_id_promociones, int cantidad) {
+        this.pedido_id_pedido = pedido_id_pedido;
+        this.producto_id_producto = producto_id_producto;
+        this.promociones_id_promociones = promociones_id_promociones;
+        this.cantidad = cantidad;
+    }
+
+    public Detalle_Pedido(int id_det_pedido, int pedido_id_pedido, int producto_id_producto, int promociones_id_promociones, int cantidad) {
+        this.id_det_pedido = id_det_pedido;
+        this.pedido_id_pedido = pedido_id_pedido;
+        this.producto_id_producto = producto_id_producto;
+        this.promociones_id_promociones = promociones_id_promociones;
+        this.cantidad = cantidad;
+    }
+
+    public Detalle_Pedido() {
+    }
+
+    public int getId_det_pedido() {
+        return id_det_pedido;
+    }
+
+    public void setId_det_pedido(int id_det_pedido) {
+        this.id_det_pedido = id_det_pedido;
+    }
+
+    public int getPedido_id_pedido() {
+        return pedido_id_pedido;
+    }
+
+    public void setPedido_id_pedido(int pedido_id_pedido) {
+        this.pedido_id_pedido = pedido_id_pedido;
+    }
+
+    public int getProducto_id_producto() {
+        return producto_id_producto;
+    }
+
+    public void setProducto_id_producto(int producto_id_producto) {
+        this.producto_id_producto = producto_id_producto;
+    }
+
+    public int getPromociones_id_promociones() {
+        return promociones_id_promociones;
+    }
+
+    public void setPromociones_id_promociones(int promociones_id_promociones) {
+        this.promociones_id_promociones = promociones_id_promociones;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Detalle_Pedido{" + "id_det_pedido=" + id_det_pedido + ", pedido_id_pedido=" + pedido_id_pedido + ", producto_id_producto=" + producto_id_producto + ", promociones_id_promociones=" + promociones_id_promociones + ", cantidad=" + cantidad + '}';
+    }
+    
     
 }
